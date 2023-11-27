61cc487fc9b89c1e250f54cb8cbf4e7c30e1d293
Bartosz Buczek <bartosz.buczek@u2i.com>
2023-11-27 10:44:56 +0100

    adds css styles

--- lib/lounge_car/engine.rb
+++ lib/lounge_car/engine.rb
@@ -1,5 +1,9 @@
 module LoungeCar
   class Engine < ::Rails::Engine
     isolate_namespace LoungeCar
+
+    initializer 'lounge_car.assets.precompile' do |app|
+      app.config.assets.precompile += %w[lounge_car/application.css]
+    end
   end
 end
