7ac16f980b2bfd456c419b66eca30de390f85352
Bartosz Buczek <bartosz.buczek@u2i.com>
2024-01-04 11:45:24 +0100

    refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 LoungeCar::Engine.routes.draw do
-  # root to: "chats#index"
-
-  resources :chats, only: %i[new show create] do
+  resources :chats, only: %i[show create] do
     post 'send_message', on: :member
   end
 end
--- lib/generators/lounge_car/templates/chat_model_template.rb
+++ lib/generators/lounge_car/templates/chat_model_template.rb
@@ -4,4 +4,13 @@ class Chat < ApplicationRecord
   include LoungeCar::Chat
 
   has_many :messages, dependent: :destroy
+
+  # def configuration_message
+  #   <<~CONFIG
+  #     ere you can write any configuration message that should be sent at the beginning of every chat.
+  #     For example:
+  #     You are a helpful assistant in grocery shop.
+  #     Today is #{Date.today}
+  #   CONFIG
+  # end
 end
