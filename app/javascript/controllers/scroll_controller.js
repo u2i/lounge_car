46667fc74fcf2744659da70f4b8bd81e8c980de2
Bartosz Buczek <bartosz.buczek@u2i.com>
2024-01-09 14:00:41 +0100

    adds throttling and delay

--- app/javascript/controllers/scroll_controller.js
+++ app/javascript/controllers/scroll_controller.js
@@ -1,13 +1,22 @@
-import {Controller} from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   connect() {}
 
-  scrollToBottom(event) {
+  lastScrollTime = Date.now();
+
+  scroll = () => {
     const messagesContainer = document.getElementById("messages");
     const max_scroll = messagesContainer.scrollHeight - messagesContainer.clientHeight;
     if (max_scroll - 200 < messagesContainer.scrollTop) {
-      messagesContainer.scrollTop = max_scroll;
+      messagesContainer.scrollTop = messagesContainer.scrollHeight;
+    }
+  }
+
+  throttledScroll = () => {
+    if (this.lastScrollTime + 1000 < Date.now()) {
+      this.lastScrollTime = Date.now();
+      setTimeout(this.scroll, 50);
     }
   }
 }
